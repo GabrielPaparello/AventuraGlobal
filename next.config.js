00f031ec3589b88784c6ce95f457c4fcc4370d26
ghapzh <gabriel.alejandro.paparello@gmail.com>
2025-05-12 12:50:13 +0200

    fixes

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // Define los dominios o patrones permitidos para cargar imágenes de manera remota
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'randomuser.me',
-        pathname: '/api/portraits/men/*',
+        hostname: 'randomuser.me',  // Dominio que usas para las imágenes
+        pathname: '/api/portraits/men/*',  // Ruta para imágenes de hombres
       },
       {
         protocol: 'https',
-        hostname: 'randomuser.me',
-        pathname: '/api/portraits/women/*',
+        hostname: 'randomuser.me',  // Dominio que usas para las imágenes
+        pathname: '/api/portraits/women/*',  // Ruta para imágenes de mujeres
       },
     ],
   },
-}
+};
 
 module.exports = nextConfig;
